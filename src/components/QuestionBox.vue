eb8c07af520b314cc8720c370cf5c7e625de077e
varun mara <varungowde@gmail.com>
2019-09-23 02:54:04 -0500

    working on fancy quiz app

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="question-box-container">
+    <b-jumbotron>
+      <template v-slot:lead>{{ currentQuestion.question}}</template>
+
+      <hr class="my-4" />
+
+      <p>List of answers</p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button @click="next" variant="success" href="#">Next</b-button>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    currentQuestion: Object,
+    next: Function
+  }
+};
+</script>
